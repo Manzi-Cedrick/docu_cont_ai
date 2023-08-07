d6b9b567c5942ecb24afa880018bbbdc3b5ad380
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 18:57:33 +0200

    [add]: not yet done update

---
+++ src/components/shared/UpdatesSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {UpdateSectionsIcon} from '../../icons'
+
+const UpdatesSection = () => {
+    return (
+        <div className='flex flex-col items-center'>
+            <UpdateSectionsIcon />
+            <div>
+                <p className='font-semibold text-4xl text-[#00000046]'>Need Design Support !</p>
+            </div>
+        </div>
+    )
+}
+
+export default UpdatesSection
