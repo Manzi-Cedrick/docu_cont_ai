48a402dc8cc38fd7175e593ee8b5113bc6e83c7f
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 19:04:08 +0200

    feat: 🚧 overview agency data header addition

---
+++ src/components/shared/TableData/AgencyOverviewHeader.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SortableFilterIcon } from '../../../icons'
+
+const AgencyOverviewHeader = () => {
+    return (
+        <thead className="bg-gray-100 ">
+            <tr>
+                <th scope="col" className="py-3.5 px-4 text-sm font-medium text-left rtl:text-right text-black">
+                    <div className="flex items-center gap-x-3">
+                        <input type="checkbox" className="text-blue-500 border-gray-300 rounded-md"/>
+                        <button className="flex items-center gap-x-2">
+                            <span>ID</span>
+                            <SortableFilterIcon/>
+                        </button>
+                    </div>
+                </th>
+                <th scope="col" className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-black">
+                    NAME
+                </th>
+                <th scope="col" className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-black">
+                    CIF/DNI
+                </th>
+                <th scope="col" className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-black">
+                    No of Users
+                </th>
+                <th scope="col" className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-black">
+                    Client Profile
+                </th>
+            </tr>
+        </thead>
+    )
+}
+
+export default AgencyOverviewHeader
