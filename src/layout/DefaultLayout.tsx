20b5297730368720a58bc087f6bcb4047c00de1e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 21:23:24 +0200

    [fix]: referencing project

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/shared/Sidebar';
-import Header from '../components/shared/Header';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
 
 const DefaultLayout = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
