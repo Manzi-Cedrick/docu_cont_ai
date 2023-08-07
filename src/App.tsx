dfd8224c30b55b6b7b37c6fecf0dd3f2feacb654
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 18:56:31 +0200

    [add]: route addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React, { lazy, useEffect, useState } from 'react';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './containers/admin/dashboard';
 import Signup from './containers/auth/signup';
 import Login from './containers/auth/login';
-import Loader from './components/shared/loader';
+import Loader from './components/shared/Loader';
+import BillingPage from './containers/admin/billingPage';
+import UserAgency from './containers/admin/UserAgency';
+import AccountsPage from './containers/admin/accountsPage';
+import TaskPage from './containers/admin/taskPage';
 
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 function App() {
@@ -13,6 +17,29 @@ function App() {
     setTimeout(() => setLoading(false), 1000);
   }, []);
 
+  const routes = [
+    {
+      path: '/admin/dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/admin/user',
+      component: UserAgency,
+    },
+    {
+      path: '/admin/billing',
+      component: BillingPage,
+    },
+    {
+      path: '/admin/accounts',
+      component: AccountsPage,
+    },
+    {
+      path: '/admin/tasks',
+      component: TaskPage,
+    },
+  ];
+
   return loading ? (
     <Loader />
   ) : (
@@ -20,8 +47,7 @@ function App() {
       <Route path="/auth/signup" element={<Signup />} />
       <Route path="/" element={<Login />} />
       <Route element={<DefaultLayout />}>
-        <Route index path="/admin/dashboard" element={<Dashboard />} />
-        {/* {routes.map(({ path, component: Component }) => (
+        {routes.map(({ path, component: Component }) => (
             <Route
               path={path}
               element={
@@ -30,7 +56,7 @@ function App() {
                 </Suspense>
               }
             />
-          ))} */}
+          ))}
       </Route>
    </Routes>
   );
