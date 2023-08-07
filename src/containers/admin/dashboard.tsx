5e4b20d5d505af903887a98fa320f3f88c2114d7
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 19:06:02 +0200

    feat: 🚧 option card referencing

--- src/containers/admin/dashboard.tsx
+++ src/containers/admin/dashboard.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import Sidebar from '../../components/shared/sidebar'
-import {FaFileCsv} from 'react-icons/fa'
 import { optionCards } from '../../utils/contants'
-import TableBody from '../../components/shared/TableBody'
+import AgencyAdminTable from '../../components/shared/pageComponents/AgencyAdminTable'
 
 const Dashboard = () => {
     return (
-        <div className=''>
+        <div>
             <div>
                 <h1 className='font-semibold text-xl'>Overview</h1>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 place-content-stretch lg:grid-cols-4 gap-6 py-6'>
                 {optionCards.map((card, index) => (
-                  <div key={index} className='bg-white rounded-lg shadow-xl flex flex-col p-5 border-2 border-primary border-solid'>
+                  <div key={index} className='bg-white rounded-lg flex flex-col p-5 border-2 border-primary border-solid'>
                     <div className='flex gap-4 place-items-center pb-4'>
                         <div className='p-5 text-primary border-2 border-primary border-solid rounded-full text-linearG'>
                             {card.icon}
@@ -22,7 +20,7 @@ const Dashboard = () => {
                             <p className='text-sm text-slate-500'>No of {card.title}</p>
                         </div>
                     </div>
-                    <div className=' border-t-2 border-[#00000012] border-solid flex '>
+                    <div className='border-t-2 border-[#00000012] border-solid flex '>
                         <div className='border-r-2 w-1/2 flex flex-col py-2 border-[#00000012] border-solid place-items-center'>
                             <h1 className='text-black font-bold text-sm'>
                                 <span className='text-primary'>+</span>{card.week_info}%</h1>
@@ -42,7 +40,7 @@ const Dashboard = () => {
                 <p className='font-normal text-slate-500 text-sm'>All data containing the agency clients</p>
             </div>
             <div className='py-10'>
-              <TableBody/>
+              <AgencyAdminTable/>
             </div>
         </div>
     )
