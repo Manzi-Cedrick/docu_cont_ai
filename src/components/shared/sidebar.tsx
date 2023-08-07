1fd6f43cec166adba8e54d2075b2a61ad1d29074
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 18:57:12 +0200

    [add]: check header active link t

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -3,6 +3,13 @@ import {Link} from 'react-router-dom'
 import {sideBarLinks} from '../../utils/contants'
 
 const Sidebar = () => {
+    const checkHeaderActive = (path: string) => {
+        if (window.location.pathname === path) {
+            return 'bg-[#00b19c20] text-primary '
+        } else {
+            return ''
+        }
+    }
     return (
         <aside className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l">
             <Link to="#">
@@ -12,7 +19,7 @@ const Sidebar = () => {
                 <nav>
                     <div className='flex flex-col gap-4'>
                     {sideBarLinks.slice(0,5).map((link) => (
-                        <Link className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md " to={link.path}>
+                        <Link className={`${checkHeaderActive(link.path)} flex items-center  px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md`} to={link.path}>
                             {link.icon}
                             <span className="mx-4 font-medium">{link.name}</span>
                         </Link>
@@ -21,7 +28,7 @@ const Sidebar = () => {
                     <hr className="my-6 border-gray-200 "/>
                     <div className='flex flex-col gap-4'>
                     {sideBarLinks.slice(5).map((link) => (
-                        <Link className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md " to={link.path}>
+                        <Link className={`${checkHeaderActive(link.path)} flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md`} to={link.path}>
                             {link.icon}
                             <span className="mx-4 font-medium">{link.name}</span>
                         </Link>
