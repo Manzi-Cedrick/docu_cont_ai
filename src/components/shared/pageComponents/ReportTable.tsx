544fbacdc2dcdd6d5ac67d2ed0b61cb263261e75
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 20:50:33 +0200

    feat: 🚧 reports table addition

---
+++ src/components/shared/pageComponents/ReportTable.tsx
@@ -0,0 +1,45 @@
+import ReportTableHeader from "../TableData/ReportTableHeader";
+import ReportTableDataCell from "../TableData/ReprortTableDataCell";
+import TablePagination from "../TableData/TablePagination";
+import UserAdminDataCell from "../TableData/UserAdminDataCell";
+import UserAdminHeader from "../TableData/UserAdminHeader";
+
+const ReportTable = () => {
+    return (
+        <section className="containe">
+            <div className="flex flex-col">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div className="overflow-hidden border border-gray-200  md:rounded-lg">
+                            <div className="flex flex-row px-4 bg-white py-4 items-center justify-start gap-4">
+                                <div className="h-10">
+                                    <select className="border-2 border-bdr bg-transparent h-full px-4 rounded-md">
+                                        <option value="#">CIF/DNI</option>
+                                        <option value="#">CIF 1</option>
+                                        <option value="#">CIF 2</option>
+                                        <option value="#">CIF 3</option>
+                                    </select>
+                                </div>
+                                <div className="h-10">
+                                    <input className="border-2 border-bdr bg-transparent focus:border-primary h-full rounded-md px-4" type="date"/>
+                                </div>
+                            </div>
+                            <table className="min-w-full divide-y divide-gray-200 ">
+                                <ReportTableHeader/>
+                                <tbody className="bg-white divide-y divide-gray-200 ">
+                                    <ReportTableDataCell data={"data"}
+                                        status={"active"}/>
+                                    <ReportTableDataCell data={"data"}
+                                        status={"inactive"}/>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <TablePagination/>
+        </section>
+    );
+};
+
+export default ReportTable;
