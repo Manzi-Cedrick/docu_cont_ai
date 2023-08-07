846a55f6a7efda9a5d7c4c2addf9b706547077d6
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 20:52:46 +0200

    feat: 🚧 user agency addition

--- src/containers/admin/UserAgency.tsx
+++ src/containers/admin/UserAgency.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import AgencyAdminTable from '../../components/shared/pageComponents/AgencyAdminTable'
 import UserAdminTable from '../../components/shared/pageComponents/UserAdminTable'
+import PageHeader from '../../components/shared/pageComponents/PageHeader'
 
 const UserAgency = () => {
     return (
         <div>
-            <div>
-                <h1 className='font-semibold text-xl'>User Agency</h1>
-                <p className='font-normal text-slate-500 text-sm'>All data containing the agency clients</p>
-            </div>
+            <PageHeader header='User Statistics' subheader='All data containing the user of the agency'/>
             <div className='py-10'>
                 <UserAdminTable/>
             </div>
