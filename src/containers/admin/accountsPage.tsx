6f9a35f0143ce4e7396c0273c2e40d5493022b67
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-08-07 20:52:35 +0200

    feat: 🚧 core accounts page addition

--- src/containers/admin/accountsPage.tsx
+++ src/containers/admin/accountsPage.tsx
@@ -2,17 +2,24 @@ import React from 'react'
 import UpdatesSection from '../../components/shared/UpdatesSection'
 import SectionAccountSSettings from '../../components/shared/pageComponents/SettingsAccountSection'
 import SectionAccountProfile from '../../components/shared/pageComponents/SettingsAccountProfile'
+import PageHeader from '../../components/shared/pageComponents/PageHeader'
 
 const AccountsPage = () => {
     return (
-        <div className="flex flex-wrap">
-            <div className="w-full lg:w-8/12 px-4">
-                <SectionAccountSSettings/>
-            </div>
-            <div className="w-full lg:w-4/12 px-4">
-                <SectionAccountProfile/>
+        <div>
+            <PageHeader header='Accounts' subheader='All data containing the accounts of the admin'/>
+            <div className='py-10'>
+                <div className="flex flex-wrap">
+                    <div className="w-full lg:w-8/12 px-4">
+                        <SectionAccountSSettings/>
+                    </div>
+                    <div className="w-full lg:w-4/12 px-4">
+                        <SectionAccountProfile/>
+                    </div>
+                </div>
             </div>
         </div>
+
     )
 }
 
